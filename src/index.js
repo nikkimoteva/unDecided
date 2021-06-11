6f536c6e0d57a95fe82366f3fd8b96a6edb41a0b
kians376 <kianshah376@gmail.com>
2021-06-10 20:52:31 -0700

    Added structure

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Docs from "./Docs";
+import SignUp from "./SignUp";
+import Demo from "./Demo";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Switch>
+        <Route path="/signup"><SignUp/></Route>
+        <Route path="/docs"><Docs/></Route>
+        <Route path="/demo"><Demo/></Route>
         <Route path="/"><App/></Route>
       </Switch>
     </Router>
