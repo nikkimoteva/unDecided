3e995886390fb25de3c2fa653cdf2faba0b498d2
kians376 <kianshah376@gmail.com>
2021-06-11 14:57:52 -0700

    Added header and login modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
 import Header from "./Header";
+import {useState} from "react";
+import LoginModal from "./LoginModal";
+import {Modal} from "@material-ui/core";
 
 function App() {
+  const [login, setLogin] = useState(false);
+  function openLoginModal() {
+    setLogin(true);
+  }
+
+  function closeLoginModal() {
+    setLogin(false);
+  }
+
   return (
     <>
-      <Header/>
+      <Modal
+        open={login}
+        onClose={closeLoginModal}
+        aria-labelledby="Login Form"
+        aria-describedby="Input your login details here"
+      >
+        <LoginModal/>
+      </Modal>
+      <Header handleLogin={openLoginModal}/>
       <div className="App">
         <h1>State Of The Art</h1>
         <h1>Automated</h1>
