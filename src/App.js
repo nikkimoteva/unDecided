40da56c42690a04b752e3dc15cf0d14cc5e3bd41
Nikki <nikkimoteva@gmail.com>
2021-06-11 19:01:16 -0700

    added designs to the front page, including some stylings

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import './App.css';
 import Header from "./Header";
+import React, { useState, useEffect } from 'react';
+import Typed from 'react-typed';
+import logo from './logo.png';
+import Button from "./Button"
+import "./Button.css"
 
 function App() {
+
+  let keywords = ["autoML", "Automated Machine Learning", "UBC", "State Of The Art"];
+  let buttonSignup = {
+    name: "Get Started",
+    route: "./signup"
+  }
+  
+  let buttonDemo = {
+    name: "autoML In Action",
+    route: "./demo"
+  }
+
   return (
-    <>
-      <Header/>
-      <div className="App">
-        <h1>State Of The Art</h1>
-        <h1>Automated</h1>
-        <h1>Machine</h1>
-        <h1>Learning</h1>
-      </div>
-    </>
-  );
+    <div className="HeaderLogo">
+        <img className="initialLogo" src={logo} alt="logo" />
+        <br/>
+        <Typed className="keywords"
+            strings={keywords}
+            typeSpeed={50}
+            startDelay= {500}
+            backSpeed= {60}
+            showCursor= {true}
+            cursorChar= {'|'}
+            autoInsertCss= {true}
+            shuffle= {true}
+            smartBackspace= {false}
+            loop= {true}
+        />
+        <br/>
+        <div className="ButtonUI">
+          <Button data={buttonDemo} />
+          <Button data={buttonSignup} />
+        </div>
+    </div>
+);
 }
+
 
 export default App;
