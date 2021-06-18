ed4680e5afd9b505d3c7893d6fc9657ef25ba192
kians376 <kianshah376@gmail.com>
2021-06-18 16:54:10 -0700

    Added styling

--- src/Docs/Docs.js
+++ src/Docs/Docs.js
@@ -1,5 +1,15 @@
+import React from "react";
+import {Switch, Route, useRouteMatch} from "react-router-dom";
 
 
 export default function Docs() {
+  const { path, url } = useRouteMatch();
 
+  // Use this to create pages based off of /docs part of url
+  return <Switch>
+    <Route path={`${path}/Page1`}><></></Route>
+    <Route path={`${path}/Page2`}><></></Route>
+    <Route path={`${path}/Page3`}><></></Route>
+    <Route path={path}><></></Route>  {/*Default route*/}
+  </Switch>
 }
--- src/index.js
+++ src/index.js
@@ -3,21 +3,48 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Landing Page/App';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {createMuiTheme, makeStyles, ThemeProvider, StylesProvider} from '@material-ui/core/styles';
 import Docs from "./Docs/Docs";
 import SignUp from "./Sign Up/SignUp";
 import Demo from "./Demo/Demo";
+import {responsiveFontSizes} from "@material-ui/core";
+import {green} from "@material-ui/core/colors";
+
+
+let theme = createMuiTheme({
+  // See https://material-ui.com/customization/theming/
+
+  // color palette
+  palette: {
+    type: "dark",
+    divider: "rgba(255, 255, 255, 0.12)",
+  },
+  // fonts and font sizes
+  typography: {
+
+  },
+  //default component props, like spacing, ripple effect, etc
+  props: {},
+  // all other component styles
+  overrides: {}
+});
+
+theme = responsiveFontSizes(theme);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Switch>
-        
-        <Route path="/signup"><SignUp/></Route>
-        <Route path="/docs"><Docs/></Route>
-        <Route path="/demo"><Demo/></Route>
-        <Route path="/"><App/></Route>
-      </Switch>
-    </Router>
+      <ThemeProvider theme={theme}>
+        <StylesProvider injectFirst>
+          <Router>
+            <Switch>
+              <Route path="/signup"><SignUp/></Route>
+              <Route path="/docs"><Docs/></Route>
+              <Route path="/demo"><Demo/></Route>
+              <Route path="/"><App/></Route>
+            </Switch>
+          </Router>
+        </StylesProvider>
+      </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
