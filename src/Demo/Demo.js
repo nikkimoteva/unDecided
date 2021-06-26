f78571758fe0392c5e7edf55ba83feeeb0d20563
kians376 <kianshah376@gmail.com>
2021-06-25 18:49:24 -0700

    Removed Demo header

--- src/Demo/Demo.js
+++ src/Demo/Demo.js
@@ -97,30 +97,30 @@ export default function PersistentDrawerLeft() {
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <AppBar
-        style={{ background: '#2E3B55' }}
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
-        })}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            className={clsx(classes.menuButton, open && classes.hide)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap>
-            Demo
-          </Typography>
-          <Button alignItems="right" data={{name: "back to main page",
-                route: "./App"}} />
-        </Toolbar>
-      </AppBar>
+      {/*<AppBar*/}
+      {/*  style={{ background: '#2E3B55' }}*/}
+      {/*  position="fixed"*/}
+      {/*  className={clsx(classes.appBar, {*/}
+      {/*    [classes.appBarShift]: open,*/}
+      {/*  })}*/}
+      {/*>*/}
+      {/*  <Toolbar>*/}
+      {/*    <IconButton*/}
+      {/*      color="inherit"*/}
+      {/*      aria-label="open drawer"*/}
+      {/*      onClick={handleDrawerOpen}*/}
+      {/*      edge="start"*/}
+      {/*      className={clsx(classes.menuButton, open && classes.hide)}*/}
+      {/*    >*/}
+      {/*      <MenuIcon />*/}
+      {/*    </IconButton>*/}
+      {/*    <Typography variant="h6" noWrap>*/}
+      {/*      Demo*/}
+      {/*    </Typography>*/}
+      {/*    <Button alignItems="right" data={{name: "back to main page",*/}
+      {/*          route: "./App"}} />*/}
+      {/*  </Toolbar>*/}
+      {/*</AppBar>*/}
       <Drawer
         className={classes.drawer}
         variant="persistent"
