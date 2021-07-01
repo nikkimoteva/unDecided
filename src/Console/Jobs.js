46d0f8010924ee63dd56f0b21b3c3ea84853e429
RobertMo <robertmozhaoming@gmail.com>
2021-07-01 02:03:20 -0700

    change Jobs table layout

--- src/Console/Jobs.js
+++ src/Console/Jobs.js
@@ -10,6 +10,7 @@ import React from "react";
 
 const useStyles = makeStyles({
   submitJobButton: {
+    float:"left",
     display: "flex",
     justify: "flex-end",
     margin: "auto",
@@ -23,25 +24,26 @@ export default function Jobs(props) {
 
   return (
     <div>
-      <TableContainer component={Paper}>
-        <Table aria-label="collapsible table">
-          <TableHead>
-            <TableRow>
-              <TableCell />
-              <TableCell>Job ID</TableCell>
-              <TableCell align="right">Status</TableCell>
-              <TableCell align="right">Starting Date</TableCell>
-              <TableCell align="right">Finishing Date</TableCell>
-            </TableRow>
-          </TableHead>
-        </Table>
-      </TableContainer>
       <Button variant="contained" color="secondary"
               className={classes.submitJobButton} component={Link}
               to={`${props.url}/submitJob`}
       >
         New Job
       </Button>
+      <TableContainer component={Paper}>
+        <Table aria-label="collapsible table">
+          <TableHead>
+            <TableRow>
+              <TableCell />
+              <TableCell>Job ID</TableCell>
+              <TableCell align="right">Job Name</TableCell>
+              <TableCell align="right">Description</TableCell>
+              <TableCell align="right">Status</TableCell>
+            </TableRow>
+          </TableHead>
+        </Table>
+      </TableContainer>
+      
     </div>
   );
 }
