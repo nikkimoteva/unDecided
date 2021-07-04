437793a80e088883834342da430e83f7e34da7b6
kians376 <kianshah376@gmail.com>
2021-07-03 22:56:59 -0700

    Some stuff

--- server/Server.js
+++ server/Server.js
@@ -4,11 +4,11 @@ const {ListBucketsCommand, ListObjectsCommand, GetObjectCommand, S3Client} = req
 const {storeCSV} = require("./FileManager");
 const path = require('path');
 
+const auth = require("./Auth.js");
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const logger = require('morgan');
-const axios = require('axios');
 const port = 3001;
 
 let awsClient = null;
@@ -39,32 +39,9 @@ app.get("/jobs", (req, res) => {
 });
 
 app.post("/gauth", (req, res) => {
-  const userToken = req.body.id_token;
-  // not really needed unless we go into actual production.
-  // verify(userToken)
-  //   .then((userId) => {
-  //     const profile = req.body.profile;
-  //     const name = profile.name;
-  //     const imageUrl = profile.imageUrl;
-  //     const email = profile.email; // This is null if the 'email' scope is not present.
-  //     // TODO store profile information in db
-  //
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //     res.sendStatus(401);
-  //   })
-  axios.get(`https://oauth2.googleapis.com/tokeninfo?id_token=${userToken}`)
-    .then(axiosRes => {
-      if (axiosRes.status !== 200) throw new Error();
-      const body = axiosRes.data;
-      const name = body.name;
-      const picture = body.picture;
-      const email = body.email;
-      // TODO store profile information in db
-      res.sendStatus(200);
-    })
-    .catch(err => errorHandler(err, res));
+  const success = auth.verifyAuth(req);
+  const responseCode = (success) ? 200 : 400;
+  res.sendStatus(responseCode);
 });
 
 app.post("/submitJob", (req, res) => {
