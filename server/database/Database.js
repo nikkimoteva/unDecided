b1328c20a7af3d4ff9bbf1e840ea5e934ddc05e2
kians376 <kianshah376@gmail.com>
2021-07-05 16:10:03 -0700

    Final commit

--- server/database/Database.js
+++ server/database/Database.js
@@ -1,9 +1,5 @@
 const mongoose = require('mongoose');
-
-const user = 'blackboxAdmin';
-const password = 'hz2PWdY3ZFaThSr';
-const server = 'cluster0.lwfgf.mongodb.net';
-const database = 'blackboxML';
+const {user, password, server, database} = require("../../common");
 
 class Database {
   constructor() {
