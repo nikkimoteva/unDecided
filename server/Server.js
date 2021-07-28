a3b7e7752227c8801c3bf587a762438c9b6c2ef2
kians376 <kianshah376@gmail.com>
2021-07-28 11:26:35 -0700

    Bug fix?

--- server/Server.js
+++ server/Server.js
@@ -16,7 +16,7 @@ app.use('/api', api);
 
 app.use(express.static(path.join(__dirname, '../build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build'));
+  res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
