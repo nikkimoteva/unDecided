b62c35e955647b009e0aa9604650d9524a8af559
kians376 <kianshah376@gmail.com>
2021-07-27 22:57:09 -0700

    Bug fix?

--- src/common/Header/LoginModal.js
+++ src/common/Header/LoginModal.js
@@ -13,7 +13,7 @@ export default class LoginModal extends React.Component {
             buttonText="Login with Google"
             onSuccess={this.props.signin}
             onFailure={(res) => console.log(res)}
-            cookiePolicy="http://localhost:3000"
+            cookiePolicy="http://localhost:5000"
           />
         </div>
       </DialogContent>
