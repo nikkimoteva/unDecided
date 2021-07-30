956d56e5eea5bc9c4fdf1332d4e5ca05311227ab
aestrr <harryxxh@hotmail.com>
2021-07-29 22:13:34 -0700

    weeeeee

---
+++ src/common.js
@@ -0,0 +1,22 @@
+// Database stuff
+exports.user = "blackboxAdmin";
+exports.password = "WMJej8OPx4Ygjiue";
+exports.server = "cluster0.9d4gt.mongodb.net";
+exports.database = "blackboxml";
+
+// Email stuff
+exports.email = "eautoml@gmail.com";
+exports.captcha_site_key = "6LdhBZAbAAAAAGFhgRjjiriKkVBtDyXQ90XI9yWg";
+exports.captcha_secret_key = "6LdhBZAbAAAAANyTdPJAApbjRdHk0XELSn8GTmQd";
+exports.contactus_user = "user_MI68jfJKHkInO2WeBKtr1";
+exports.contactus_access_token = "9cbf43157b0c4fcc8392c7c8344f4c22";
+exports.contactus_service_id = "service_6d9ihy3";
+exports.contactus_template_id = "template_uuppayj";
+
+// backend job stuff
+exports.ssh_user = 'blkbx-ml';
+exports.ssh_pw = '1qaz2wsx';
+exports.borg_dataset_directory = "../../../research/plai-scratch/BlackBoxML/bbml-backend-3/ensemble_squared/datasets/";
+exports.remote_ssh = 'remote.cs.ubc.ca';
+exports.ensemble_session_path = '../../../research/plai-scratch/BlackBoxML/bbml-backend-3/ensemble_squared/sessions/';
+exports.slurm_command_dataset_path = '/ubc/cs/research/plai-scratch/BlackBoxML/bbml-backend-3/ensemble_squared/datasets/';
