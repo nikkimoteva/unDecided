224c8400eed03af53b53b05a49eb7bb2d17d0561
kians376 <kianshah376@gmail.com>
2021-08-09 15:04:29 -0700

    removed dateformat

--- src/Console/Jobs.js
+++ src/Console/Jobs.js
@@ -20,7 +20,7 @@ import Paper from '@material-ui/core/Paper';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import LinearProgress from '@material-ui/core/LinearProgress';
-import dateFormat from 'dateformat';
+// import dateFormat from 'dateformat';
 
 
 import {
@@ -171,7 +171,7 @@ export default function Jobs(props) {
               {props.name}
             </TableCell>
             <TableCell align="center">{props.status}</TableCell>
-            <TableCell align="center">{dateFormat(props.created, "mmmm dS, yyyy, h:MM:ss TT")}</TableCell>
+            <TableCell align="center">{1}</TableCell>
             <TableCell align="center">
               {
                 (props.status === "Running") ? <div style={{display: "none"}}/>
@@ -225,7 +225,7 @@ export default function Jobs(props) {
             {row.name}
           </TableCell>
           <TableCell align="right">{row.status}</TableCell>
-          <TableCell align="right">{dateFormat(row.created, "mmmm dS, yyyy, h:MM:ss TT")}</TableCell>
+          <TableCell align="right">{1}</TableCell>
           <TableCell align="right">{row.target_name}</TableCell>
           <TableCell align="center"><Button variant="contained"
             className={classes.jobActionButton} onClick={deleteJob} color="primary"
