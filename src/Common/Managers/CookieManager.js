d8d65aad7ac7a91fdcb678b8ed2a7f5f87ebd6e6
kians376 <kianshah376@gmail.com>
2021-08-09 15:28:34 -0700

    niasjda

--- src/Common/Managers/CookieManager.js
+++ src/Common/Managers/CookieManager.js
@@ -1,5 +1,5 @@
-// import Cookies from 'universal-cookie';
-import {Cookies} from 'react-cookie';
+import Cookies from 'universal-cookie';
+// import {Cookies} from 'react-cookie';
 
 
 let _cookies;
